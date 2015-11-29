d76d9e2030cb1fdba5f619d45c4404400a9582db
Rohinton Kazak <rohinton.kazak@gmail.com>
2015-11-28 17:05:56 -0800

    refactor middleware

---
+++ middleware.js
@@ -0,0 +1,17 @@
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log(			'Request: ' +
+					  new Date().toString() +
+					  ' '                   +
+					  req.method            +
+					  ' '                   +
+					  req.path );
+		next();
+	}
+};
+
+module.exports  = middleware;
